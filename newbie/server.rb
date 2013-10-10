bc3d968ab86cc75b3c4070df8b281d28bdcbcd42
DeathKing <DeathKing0622@gmail.com>
2013-10-10 21:36:31 +0800

    a communicate spec add.

--- newbie/server.rb
+++ newbie/server.rb
@@ -2,3 +2,17 @@
 
 require 'socket'
 require_relative 'lib/database.rb'
+
+port = 6222
+
+puts "[#{Time.now}] FoOTOo newbies server start on #{port}."
+server = TCPServer.new(port)
+
+while (session = server.accept)
+	Thread.start do
+		puts "[#{Time.now}] Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}."
+		input = session.gets
+		puts "[#{Time.new}] Client puts #{input}."
+		session.puts "Server: Welcom #{session.peeraddr[2]}."
+	end
+end	
