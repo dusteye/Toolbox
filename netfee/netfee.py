7e5fbcef6cc0240ed1e0cf96c997deeaba84f5e4
dusteye <dusteye@dusteye-vm.(none)>
2013-11-15 14:01:31 +0800

    read config from config.json

--- netfee/netfee.py
+++ netfee/netfee.py
@@ -1,71 +1,100 @@
 #!/usr/bin/python3
-#coding:utf-8
 
 import urllib.request
 import urllib.parse
 import http.cookiejar
 import smtplib
+import sys
+import json
 
 from email.mime.text import MIMEText
 from email.header import Header
 
-user = 'footoo.notifications@gmail.com'
-passwd = 'thepassword'
-to = 'cliffwoo@gmail.com'
+class Monitor:
+	def __init__(self, configpath):
+		self.configfile = configpath
+		self.config = self.readConfig()
+		
+		self.mailserver 	= self.config['mailfrom']['mailserver']
+		self.serverport 	= self.config['mailfrom']['serverport']
+		self.fromaddress 	= self.config['mailfrom']['address']
+		self.frompasswd 	= self.config['mailfrom']['password']
+		self.toaddress 		= self.config['mailto']
+		
+		self.accounts = [(account['ip'], account['password']) for account in self.config['accounts']]
+		self.warning = False
+		
+	def readConfig(self):
+		config = {}
+		try:
+			with open(self.configfile) as f:
+				config = json.load(f)
+		except IOError:
+			print("file \'%s\' doesn't exists" % configfile, file=sys.stderr)
+			sys.exit()
+		except ValueError as err:
+			print("Illegal json format \'%s\'" % err, file=sys.stderr)
+			sys.exit()
+		return config
+	
+	def genMail(self):
+		context = ''
+		for (ip, passwd) in self.accounts:
+			balance = checkAccount(ip, passwd)
+			context += ip + ": " + balance + '\n'
+			if float(balance) < 1.0:
+				self.warning = True
+		context += '\n'
+		self.msg = MIMEText(context.encode('utf-8'), 'plain', 'utf-8')
+		sub = Header('服务器余额', 'utf-8')
+		self.msg['Subject'] = sub
+		self.msg['From'] = self.fromaddress
+		self.msg['To'] = self.toaddress
 
-def autologin(url, params, req_encoding, res_encoding):
+	def sendMail(self):
+		server = smtplib.SMTP(self.mailserver, self.serverport)
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login(self.fromaddress, self.frompasswd)
+		server.sendmail(self.fromaddress, self.toaddress, self.msg.as_string())
+		server.close()
+	
+	def run(self):
+		self.genMail()
+		if self.warning:
+			self.sendMail()
+
+def autoLogin(url, params, req_encoding):
 	cookiejar = http.cookiejar.CookieJar()
 	opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookiejar))
 	urllib.request.install_opener(opener)
-	
+
 	params = urllib.parse.urlencode(params)
 	params = params.encode(req_encoding)
 	response = urllib.request.urlopen(url, params)
 
-	text = response.read()
-	
-	text_decode = text.decode(req_encoding)
-	text = text_decode.encode(res_encoding)
-	
-	return text
+	return response
 
-def check(ip, passwd): 
+def checkAccount(ip, passwd): 
 	params = {"fr":"00", "id_ip":ip, "pass":passwd, "set":"进入"}
 	req_encoding = 'gb2312'
 	res_encoding = 'utf-8'
-	text = autologin('http://hitsun.hit.edu.cn/index1.php', params, req_encoding, res_encoding)
+	
+	response = autoLogin('http://hitsun.hit.edu.cn/index1.php', params, req_encoding)
+	text = response.read()
+
+	text_decode = text.decode(req_encoding)
+	text = text_decode.encode(res_encoding)
+
 	text = str(text, 'utf-8')
 	search_text = '所剩余额'
+	
 	for line in text.splitlines():
 		if line.find(search_text)!=-1:
 			return(line.split(';')[2].split(' ')[0])
 
-def genMail(iplist, user, to):
-	context = ''
-	for (ip,passwd) in iplist.items():
-		context += ip + ": " + check(ip, passwd) + '\n'
-	context += '\n'
-	msg = MIMEText(context.encode('utf-8'), 'plain', 'utf-8')
-	sub = Header('当月服务器余额情况', 'utf-8')
-	msg['Subject'] = sub
-	msg['From'] = user
-	msg['To'] = to
-	return msg
-
-def sendMail(From, FromPass, To, mail):
-	if not mail:
-		return
-	server = smtplib.SMTP("smtp.gmail.com", 587)
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	server.login(From, FromPass)
-	server.sendmail(From, To, mail)
-	server.close()
 
 if __name__ == '__main__':
-	iplist = {	'202.118.239.46':'thepassword', 
-				'202.118.250.18':'thepassword', 
-				'202.118.250.19':'thepassword'}
-	mail = genMail(iplist, user, to)
-	sendMail(user, passwd, to, mail.as_string())
+	monitor = Monitor('config.json')
+	monitor.run()
